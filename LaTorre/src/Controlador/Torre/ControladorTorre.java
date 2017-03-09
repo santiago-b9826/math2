1bf3398fa4b1e7dd81abb2a14556445b055ef657
santib98 <santiago.bedoya5@udea.edu.co>
2017-03-08 23:03:19 -0500

    De digitos a letras

--- LaTorre/src/Controlador/Torre/ControladorTorre.java
+++ LaTorre/src/Controlador/Torre/ControladorTorre.java
@@ -14,5 +14,66 @@ public class ControladorTorre {
         boolean c = centenas.getText().equals(numero.getText().substring(0, 1));
         return (u && d && c);
     }
+    public String[] centenas() {
+        String[] centenas = {"Ciento", "Cien", "Doscientos", "Trescientos", "Cuatrocientos",
+            "Quinientos", "Seiscientos", "Setecientos", "Ochocientos", "Novecientos"};
+        return centenas;
+    }
+
+    public String[] decenas() {
+        String[] decenas = {"diez", "once", "doce", "trece", "catorce", "quince", "dieci", "veinte",
+            "veinti", "treinta", "cuarenta", "ciencuenta", "sesenta", "setenta", "ochenta", "noventa"};
+        return decenas;
+    }
+
+    public String[] unidades() {
+        String[] unidades = {"", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"};
+        return unidades;
+    }
+
+    public String numeros(int a) {
+        int u = a % 10;
+        int d = (a / 10) % 10;
+        int c = a / 100;
+        String numeroLetra;
+        String[] centenas = centenas();
+        String[] decenas = decenas();
+        String[] unidades = unidades();
+
+        if (u == 0 && d == 0) {
+            numeroLetra = centenas[c];
+            return numeroLetra + ".";
+        }
+        numeroLetra = centenas[c] + " ";
+        if (c == 1) {
+            numeroLetra = centenas[0] + " ";
+        }
+        if (d == 0) {
+            numeroLetra += unidades[u];
+            return numeroLetra + ".";
+        }
+        if (d == 1) {
+            if (u <= 5) {
+                numeroLetra += decenas[u];
+            } else {
+                numeroLetra += decenas[6] + unidades[u];
+            }
+            return numeroLetra + ".";
+        }
+        if (d == 2) {
+            if (u == 0) {
+                numeroLetra += decenas[7];
+            } else {
+                numeroLetra += decenas[d + 6] + unidades[u];
+            }
+            return numeroLetra + ".";
+        }
+        if (u == 0) {
+            numeroLetra += decenas[d + 6];
+        } else {
+            numeroLetra += decenas[d + 6] + " y " + unidades[u];
+        }
+        return numeroLetra + ".";
+    }
 
 }
