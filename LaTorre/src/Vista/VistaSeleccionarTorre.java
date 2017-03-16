06e4c68ab32511a08f15dac686ed1db55551bfe5
auxlis <auxlis@auxlis-OptiPlex-780>
2017-03-16 11:46:42 -0500

    Just vista :c

--- LaTorre/src/Vista/VistaSeleccionarTorre.java
+++ LaTorre/src/Vista/VistaSeleccionarTorre.java
@@ -11,6 +11,8 @@ public class VistaSeleccionarTorre extends javax.swing.JFrame {
     public VistaSeleccionarTorre() {
         initComponents();
         setLocationRelativeTo(null);
+        
+        
     }
 
     @SuppressWarnings("unchecked")
@@ -19,43 +21,49 @@ public class VistaSeleccionarTorre extends javax.swing.JFrame {
 
         btnDosCifras = new javax.swing.JButton();
         btnTresCifras = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(548, 280));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        btnDosCifras.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
+        btnDosCifras.setForeground(new java.awt.Color(0, 0, 0));
         btnDosCifras.setText("Dos Cifras");
+        btnDosCifras.setBorderPainted(false);
+        btnDosCifras.setContentAreaFilled(false);
+        btnDosCifras.setOpaque(false);
         btnDosCifras.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnDosCifrasActionPerformed(evt);
             }
         });
+        getContentPane().add(btnDosCifras, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 120, 120, 70));
 
+        btnTresCifras.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
+        btnTresCifras.setForeground(new java.awt.Color(0, 0, 0));
         btnTresCifras.setText("Tres Cifras");
+        btnTresCifras.setBorderPainted(false);
+        btnTresCifras.setContentAreaFilled(false);
         btnTresCifras.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnTresCifrasActionPerformed(evt);
             }
         });
+        getContentPane().add(btnTresCifras, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 120, -1, 70));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(21, 21, 21)
-                .addComponent(btnDosCifras)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 123, Short.MAX_VALUE)
-                .addComponent(btnTresCifras)
-                .addGap(90, 90, 90))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(132, 132, 132)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnDosCifras)
-                    .addComponent(btnTresCifras))
-                .addContainerGap(145, Short.MAX_VALUE))
-        );
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/a.png"))); // NOI18N
+        jLabel1.setPreferredSize(new java.awt.Dimension(220, 110));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 120, 220, 110));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/a.png"))); // NOI18N
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 120, 220, 110));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/FondP.png"))); // NOI18N
+        jLabel2.setPreferredSize(new java.awt.Dimension(548, 280));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 548, 280));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -109,5 +117,8 @@ public class VistaSeleccionarTorre extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnDosCifras;
     private javax.swing.JButton btnTresCifras;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
 }
--- LaTorre/src/Vista/VistaTorre.java
+++ LaTorre/src/Vista/VistaTorre.java
@@ -4,7 +4,6 @@ import Controlador.ControladorTorre;
 import Controlador.ControladorTorreDosCifras;
 import Controlador.ControladorTorreTresCifras;
 import Modelo.ModeloTorre;
-import java.awt.Toolkit;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
@@ -22,6 +21,7 @@ public class VistaTorre extends javax.swing.JFrame {
     public VistaTorre(int cantidadDeCifras) {
         initComponents();
         setLocationRelativeTo(null);
+        setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 //        float escalar = 0;
 //        int ancho = (int) (Toolkit.getDefaultToolkit().getScreenSize().width * escalar);
 //        int alto = (int) (Toolkit.getDefaultToolkit().getScreenSize().height * escalar);
@@ -31,6 +31,7 @@ public class VistaTorre extends javax.swing.JFrame {
         if (cantidadDeCifras == 2) {
             ControladorTorre = new ControladorTorreDosCifras();
             txtFCentenas.setVisible(false);
+            Centenas.setVisible(false);
         } else {
             ControladorTorre = new ControladorTorreTresCifras();
         }
@@ -69,7 +70,9 @@ public class VistaTorre extends javax.swing.JFrame {
         nivel9 = new javax.swing.JLabel();
         nivel10 = new javax.swing.JLabel();
         nivel11 = new javax.swing.JLabel();
+        Centenas = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
         Fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -84,11 +87,13 @@ public class VistaTorre extends javax.swing.JFrame {
                 btnVerificarNumeroActionPerformed(evt);
             }
         });
-        getContentPane().add(btnVerificarNumero, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 320, -1, -1));
+        getContentPane().add(btnVerificarNumero, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 340, -1, -1));
         getContentPane().add(txtFCentenas, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 220, 70, -1));
         getContentPane().add(txtFDecenas, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 220, 70, -1));
         getContentPane().add(txtFUnidades, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 220, 70, -1));
 
+        txtNumero.setFont(new java.awt.Font("DejaVu Sans", 1, 14)); // NOI18N
+        txtNumero.setForeground(new java.awt.Color(0, 0, 0));
         txtNumero.setEnabled(false);
         getContentPane().add(txtNumero, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 140, 270, -1));
 
@@ -101,103 +106,115 @@ public class VistaTorre extends javax.swing.JFrame {
         getContentPane().add(btnSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 410, 150, -1));
 
         numeroNivel10.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        numeroNivel10.setForeground(new java.awt.Color(10, 255, 92));
+        numeroNivel10.setForeground(new java.awt.Color(255, 255, 255));
         numeroNivel10.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(numeroNivel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 150, 70, 20));
+        getContentPane().add(numeroNivel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 120, 70, 20));
 
         numeroNivel9.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        numeroNivel9.setForeground(new java.awt.Color(10, 255, 92));
+        numeroNivel9.setForeground(new java.awt.Color(255, 255, 255));
         numeroNivel9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(numeroNivel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 200, 70, 20));
+        getContentPane().add(numeroNivel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 160, 70, 20));
 
         numeroNivel8.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        numeroNivel8.setForeground(new java.awt.Color(10, 255, 92));
+        numeroNivel8.setForeground(new java.awt.Color(255, 255, 255));
         numeroNivel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(numeroNivel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 240, 70, 20));
+        getContentPane().add(numeroNivel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 210, 70, 20));
 
         numeroNivel7.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        numeroNivel7.setForeground(new java.awt.Color(10, 255, 92));
+        numeroNivel7.setForeground(new java.awt.Color(255, 255, 255));
         numeroNivel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(numeroNivel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 290, 70, 20));
+        getContentPane().add(numeroNivel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 250, 70, 20));
 
         numeroNivel6.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        numeroNivel6.setForeground(new java.awt.Color(10, 255, 92));
+        numeroNivel6.setForeground(new java.awt.Color(255, 255, 255));
         numeroNivel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(numeroNivel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 330, 70, 20));
+        getContentPane().add(numeroNivel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 300, 70, 20));
 
         numeroNivel5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        numeroNivel5.setForeground(new java.awt.Color(10, 255, 92));
+        numeroNivel5.setForeground(new java.awt.Color(255, 255, 255));
         numeroNivel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(numeroNivel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 380, 70, 20));
+        getContentPane().add(numeroNivel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 340, 70, 20));
 
         numeroNivel4.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        numeroNivel4.setForeground(new java.awt.Color(10, 255, 92));
+        numeroNivel4.setForeground(new java.awt.Color(255, 255, 255));
         numeroNivel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(numeroNivel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 430, 70, 20));
+        getContentPane().add(numeroNivel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 400, 70, 20));
 
         numeroNivel3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        numeroNivel3.setForeground(new java.awt.Color(10, 255, 92));
+        numeroNivel3.setForeground(new java.awt.Color(255, 255, 255));
         numeroNivel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(numeroNivel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 470, 70, 20));
+        getContentPane().add(numeroNivel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 440, 70, 20));
 
         numeroNivel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        numeroNivel2.setForeground(new java.awt.Color(10, 255, 92));
+        numeroNivel2.setForeground(new java.awt.Color(255, 255, 255));
         numeroNivel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(numeroNivel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 520, 70, 20));
+        getContentPane().add(numeroNivel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 490, 70, 20));
 
         numeroNivel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        numeroNivel1.setForeground(new java.awt.Color(10, 255, 92));
+        numeroNivel1.setForeground(new java.awt.Color(255, 255, 255));
         numeroNivel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(numeroNivel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 560, 70, 20));
+        getContentPane().add(numeroNivel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 540, 70, 20));
 
         nivel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         nivel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/nivel1O.png"))); // NOI18N
-        getContentPane().add(nivel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 550, 210, -1));
+        getContentPane().add(nivel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(155, 522, 210, -1));
 
         nivel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         nivel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/nivel2O.png"))); // NOI18N
-        getContentPane().add(nivel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(165, 500, 200, 60));
+        getContentPane().add(nivel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 470, 200, 60));
 
         nivel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         nivel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/nivel3O.png"))); // NOI18N
-        getContentPane().add(nivel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(165, 460, 200, -1));
+        getContentPane().add(nivel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 430, 200, -1));
 
         nivel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         nivel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/nivel4O.png"))); // NOI18N
-        getContentPane().add(nivel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(165, 410, 200, -1));
+        getContentPane().add(nivel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 380, 200, -1));
 
         nivel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         nivel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/nivel5O.png"))); // NOI18N
-        getContentPane().add(nivel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(165, 360, 200, -1));
+        getContentPane().add(nivel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 330, 200, -1));
 
         nivel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         nivel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/nivel6O.png"))); // NOI18N
-        getContentPane().add(nivel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(165, 320, 200, -1));
+        getContentPane().add(nivel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 290, 200, -1));
 
         nivel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         nivel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/nivel7O.png"))); // NOI18N
-        getContentPane().add(nivel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(165, 270, 200, -1));
+        getContentPane().add(nivel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 240, 200, -1));
 
         nivel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         nivel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/nivel8O.png"))); // NOI18N
-        getContentPane().add(nivel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(165, 220, 200, -1));
+        getContentPane().add(nivel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 190, 200, 50));
 
         nivel9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/nivel9O.png"))); // NOI18N
-        getContentPane().add(nivel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(175, 180, 200, 60));
+        getContentPane().add(nivel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(163, 140, 200, 60));
 
         nivel10.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         nivel10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/nivel10O.png"))); // NOI18N
-        getContentPane().add(nivel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 130, 200, 60));
+        getContentPane().add(nivel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 90, 200, 60));
 
         nivel11.setHorizontalAlignment(javax.swing.SwingConstants.TRAILING);
         nivel11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/nivel11O.png"))); // NOI18N
-        getContentPane().add(nivel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 80, 200, 60));
+        getContentPane().add(nivel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(158, 38, 200, 60));
 
-        jLabel1.setText("jLabel1");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 200, -1, -1));
+        Centenas.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
+        Centenas.setForeground(new java.awt.Color(0, 0, 0));
+        Centenas.setText("Centenas.");
+        getContentPane().add(Centenas, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 200, -1, -1));
 
-        Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/Fondo.jpg"))); // NOI18N
-        getContentPane().add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 900, 630));
+        jLabel1.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel1.setText("Decenas.");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 200, 70, -1));
+
+        jLabel2.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel2.setText("Unidades.");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 200, -1, -1));
+
+        Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/Fond.png"))); // NOI18N
+        getContentPane().add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 900, 580));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -287,10 +304,12 @@ public class VistaTorre extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Centenas;
     private javax.swing.JLabel Fondo;
     private javax.swing.JButton btnSalir;
     private javax.swing.JButton btnVerificarNumero;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel nivel1;
     private javax.swing.JLabel nivel10;
     private javax.swing.JLabel nivel11;
